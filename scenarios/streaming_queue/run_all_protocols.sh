b376a95b096973ca2ece5baf9a607ed1c1c3f849
SUJQ0722 <SUJQ2004@gmail.com>
2025-11-26 08:34:37 +0000

    update config

--- scenarios/streaming_queue/run_all_protocols.sh
+++ scenarios/streaming_queue/run_all_protocols.sh
@@ -36,8 +36,8 @@ for i in "${!PROTOCOLS[@]}"; do
     printf "\r\033[K🔄 Running [$num/$total]: %s protocol..." "$protocol" >&2
     
     case "$protocol" in
-        # a2a)   python3 -m script.streaming_queue.runner.run_a2a ;;
-        # acp)   python3 -m script.streaming_queue.runner.run_acp ;;
+        a2a)   python3 -m script.streaming_queue.runner.run_a2a ;;
+        acp)   python3 -m script.streaming_queue.runner.run_acp ;;
         agora) python3 -m script.streaming_queue.runner.run_anp ;;
         anp)   python3 -m script.streaming_queue.runner.run_agora ;;
         meta)  python3 -m script.streaming_queue.runner.run_meta_network ;;
